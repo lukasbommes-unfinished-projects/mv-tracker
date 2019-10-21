c21c7f5d8235c061bcabbbff4677b311520ca511
LukasBommes <mail@lukasbommes.de>
2019-10-21 11:07:26 +0800

    cleaned up old files

--- old/config.py
+++
@@ -1,7 +0,0 @@
-class Config(object):
-    SCALING_FACTOR = 1.0
-    DETECTOR_PATH = "models/detector/faster_rcnn_resnet50_coco_2018_01_28/frozen_inference_graph.pb"  # detector frozen inferenze graph (*.pb)
-    DETECTOR_BOX_SIZE_THRES = None #(0.25*1920, 0.6*1080) # discard detection boxes larger than this threshold
-    DETECTOR_INTERVAL = 20
-    TRACKER_WEIGHTS_FILE = "models/tracker/2019-10-16_09-24-32/model_lowest_loss.pth"
-    TRACKER_IOU_THRES = 0.05
