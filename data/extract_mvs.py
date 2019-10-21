3b95d04c548e6db3f82a69224438b12c1794d5f9
LukasBommes <mail@lukasbommes.de>
2019-10-21 17:52:08 +0800

    wrote script to extract motion vectors at different scales and wrote new dataloader

--- data/extract_mvs.py
+++ data/extract_mvs.py
@@ -1,33 +1,45 @@
+import os
 import pickle
-
+from tqdm import tqdm
 from video_cap import VideoCap
+from seqinfo import scales, frame_rates, dir_names
 
 
+CODEC = "mpeg4"  # "h264" or "mpeg4"
+DATASET = "MOT15"
 
+if __name__ == "__main__":
 
-            cap = VideoCap()
-            ret = cap.open(video_file)
-            if not ret:
-                raise RuntimeError("Could not open the video file")
+    for mode in ["train", "test"]:
+        for dir_name, frame_rate in zip(dir_names[DATASET][mode], frame_rates[DATASET][mode]):
+            current_path = os.path.join(DATASET, mode, dir_name)
+            for scale in scales:
 
-            pbar = tqdm(total=num_frames)
-            for frame_idx in range(0, num_frames):
-                ret, frame, motion_vectors, frame_type, _ = cap.read()
+                output_path = os.path.join(current_path, "mvs-{}-{}".format(CODEC, scale))
+                os.makedirs(output_path, exist_ok=True)
+
+                video_file = os.path.join(current_path, "{}-{}-{}.mp4".format(dir_name, CODEC, scale))
+
+                cap = VideoCap()
+                ret = cap.open(video_file)
                 if not ret:
-                    break
+                    raise RuntimeError("Could not open the video file")
 
-                pbar.update()
+                print("Processing video {}".format(video_file))
 
-                data_item = {
-                    "frame_idx": frame_idx,
-                    "sequence": sequence,
-                    "frame_type": frame_type,
-                }
+                frame_idx = 1
 
-                data.append(data_item)
+                while True:
+                    ret, frame, motion_vectors, frame_type, _ = cap.read()
+                    if not ret:
+                        break
 
-            cap.release()
-            pbar.close()
+                    data_item = {
+                        "motion_vectors": motion_vectors,
+                        "frame_type": frame_type,
+                    }
 
-        pickle.dump(data, open(os.path.join("preprocessed", codec, mode, "data.pkl"), 'wb'))
-        pickle.dump(lengths[mode], open(os.path.join("preprocessed", codec, mode, "lengths.pkl"), 'wb'))
+                    pickle.dump(data_item, open(os.path.join(output_path, "{:06d}.pkl".format(frame_idx)), "wb"))
+                    frame_idx += 1
+
+                cap.release()
--- data/video_write.py
+++ data/video_write.py
@@ -1,101 +1,13 @@
 # This script converts each sequence's imaged of the MOT dataset into H.264 encoded video sequences
 import os
 import subprocess
+from seqinfo import scales, frame_rates, dir_names
 
 
 CODEC = "mpeg4"  # "h264" or "mpeg4"
 DATASET = "MOT17"
 
 if __name__ == "__main__":
-
-    scales = [1.0, 0.75, 0.5]  # encode video at 100 %, 75 % and 50 % of original size
-
-    frame_rates = {
-        "MOT17": {
-                "train": [30,30,14,30,30,30,25],
-                "test": [30,30,14,30,30,30,25]
-            },
-        "MOT16": {
-            "train": [30,30,14,30,30,30,25],
-            "test": [30,30,14,30,30,30,25]
-            },
-        "MOT15": {
-            "train": [25,25,7,14,14,14,30,30,10,10,30],
-            "test": [25,7,14,14,14,5/2,30,30,10,10,30]
-            }
-    }
-
-    dir_names = {
-    "MOT17": {
-            "train": [
-                'MOT17-02-FRCNN',
-                'MOT17-04-FRCNN',
-                'MOT17-05-FRCNN',
-                'MOT17-09-FRCNN',
-                'MOT17-10-FRCNN',
-                'MOT17-11-FRCNN',
-                'MOT17-13-FRCNN',
-            ],
-            "test": [
-                'MOT17-01-FRCNN',
-                'MOT17-03-FRCNN',
-                'MOT17-06-FRCNN',
-                'MOT17-07-FRCNN',
-                'MOT17-08-FRCNN',
-                'MOT17-12-FRCNN',
-                'MOT17-14-FRCNN',
-            ]
-        },
-    "MOT16": {
-            "train": [
-                'MOT16-02',
-                'MOT16-04',
-                'MOT16-05',
-                'MOT16-09',
-                'MOT16-10',
-                'MOT16-11',
-                'MOT16-13',
-            ],
-            "test": [
-                'MOT16-01',
-                'MOT16-03',
-                'MOT16-06',
-                'MOT16-07',
-                'MOT16-08',
-                'MOT16-12',
-                'MOT16-14',
-            ]
-        },
-    "MOT15": {
-            "train": [
-                'TUD-Stadtmitte',
-                'TUD-Campus',
-                'PETS09-S2L1',
-                'ETH-Bahnhof',
-                'ETH-Sunnyday',
-                'ETH-Pedcross2',
-                'ADL-Rundle-6',
-                'ADL-Rundle-8',
-                'KITTI-13',
-                'KITTI-17',
-                'Venice-2'
-            ],
-            "test": [
-                'TUD-Crossing',
-                'PETS09-S2L2',
-                'ETH-Jelmoli',
-                'ETH-Linthescher',
-                'ETH-Crossing',
-                'AVG-TownCentre',
-                'ADL-Rundle-1',
-                'ADL-Rundle-3',
-                'KITTI-16',
-                'KITTI-19',
-                'Venice-1'
-            ]
-        }
-    }
-
 
     cwd = os.getcwd()
     for mode in ["train", "test"]:
