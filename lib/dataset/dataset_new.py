added overfit test for new dense model, fixed stat computation

lib/dataset/dataset_new.py
@@ -13,7 +13,7 @@ from lib.dataset.motion_vectors import get_vectors_by_source, get_nonzero_vector
     normalize_vectors, motion_vectors_to_image, motion_vectors_to_grid, \
     motion_vectors_to_grid_interpolated
 from lib.dataset.velocities import velocities_from_boxes
-from lib.dataset.stats import Stats
+from lib.dataset.stats import StatsMpeg4DenseStatic as Stats
 from lib.visu import draw_boxes, draw_velocities, draw_motion_vectors
 from lib.transforms.transforms import StandardizeMotionVectors, \
     StandardizeVelocities, RandomFlip, RandomMotionChange
@@ -49,6 +49,20 @@ class MotionVectorDataset(torch.utils.data.Dataset):
             provide the raw data by setting the codec in the `video_write.py`
             script accordingly and running the script.
 
+        transforms (`torchvision.transform` of `None`): Transformations which
+            are applied to the generated samples. Can be either a single
+            transformation or a several transformations composed with
+            `torchvision.transforms.Compose`. For available transformations see
+            the module `lib.transforms.transforms`. If None no transformation
+            is applied.
+
+        scales (`list` of `float`): Compose the dataset of videos with different
+            scales as specified in this list. E.g. [1.0, 0.75, 0.5] means, the
+            video snippets are available at 100 %, 75 % and 50 % of their
+            original size. Ensure that the scaled videos were created previously
+            with the `video_write.py` and `extract_mvs.py` scripts inside the
+            data folder.
+
         mvs_mode (`str`): Either "upsampled" or "dense". In "upsampled" mode
             motion vectors are encoded as image with same dimensions as the
             original video frame. In "dense" mode motion vectors are not
@@ -72,8 +86,8 @@ class MotionVectorDataset(torch.utils.data.Dataset):
 
     """
     def __init__(self, root_dir="data", mode="train", codec="mpeg4",
-        transforms=None, mvs_mode="upsampled", static_only=False,
-        exclude_keyframes=True, visu=False, debug=False):
+        transforms=None, scales=[1.0, 0.75, 0.5], mvs_mode="upsampled",
+        static_only=False, exclude_keyframes=True, visu=False, debug=False):
 
         self.DEBUG = debug  # whether to print debug information
 
@@ -96,30 +110,29 @@ class MotionVectorDataset(torch.utils.data.Dataset):
             self.sequences = {
                 "train": [
                     "MOT17/train/MOT17-02-FRCNN",  # static cam
-                    #"MOT17/train/MOT17-04-FRCNN",  # static cam
-                    #"MOT17/train/MOT17-05-FRCNN",  # moving cam
-                    #"MOT17/train/MOT17-11-FRCNN",  # moving cam
-                    #"MOT17/train/MOT17-13-FRCNN",  # moving cam
-                    #"MOT15/train/ETH-Bahnhof",  # moving cam
-                    #"MOT15/train/ETH-Sunnyday",  # moving cam
-                    #"MOT15/train/KITTI-13",  # moving cam
-                    #"MOT15/train/KITTI-17",  # static cam
-                    #"MOT15/train/PETS09-S2L1",  # static cam
-                    #"MOT15/train/TUD-Campus",  # static cam
-                    #"MOT15/train/TUD-Stadtmitte"  # static cam
+                    "MOT17/train/MOT17-04-FRCNN",  # static cam
+                    "MOT17/train/MOT17-05-FRCNN",  # moving cam
+                    "MOT17/train/MOT17-11-FRCNN",  # moving cam
+                    "MOT17/train/MOT17-13-FRCNN",  # moving cam
+                    "MOT15/train/ETH-Bahnhof",  # moving cam
+                    "MOT15/train/ETH-Sunnyday",  # moving cam
+                    "MOT15/train/KITTI-13",  # moving cam
+                    "MOT15/train/KITTI-17",  # static cam
+                    "MOT15/train/PETS09-S2L1",  # static cam
+                    "MOT15/train/TUD-Campus",  # static cam
+                    "MOT15/train/TUD-Stadtmitte"  # static cam
                 ],
                 "val": [
                     "MOT17/train/MOT17-09-FRCNN",  # static cam
-                    #"MOT17/train/MOT17-10-FRCNN"  # moving cam
+                    "MOT17/train/MOT17-10-FRCNN"  # moving cam
                 ]
             }
-
-        self.scales = [1.0, 0.75, 0.5]
 
         self.root_dir = root_dir
         self.transforms = transforms
         self.mode = mode
         self.codec = codec
+        self.scales = scales
         self.mvs_mode = mvs_mode
         self.exclude_keyframes = exclude_keyframes
         self.visu = visu
@@ -320,6 +333,9 @@ if __name__ == "__main__":
     batch_size = 1
     codec = "mpeg4"
     mvs_mode = "dense"
+    static_only = True
+    exclude_keyframes = True
+    scales = [1.0, 0.75, 0.5]
 
     transforms = {
         "train": torchvision.transforms.Compose([
@@ -337,13 +353,12 @@ if __name__ == "__main__":
     }
 
     datasets = {x: MotionVectorDataset(root_dir='data', transforms=transforms[x],
-        codec=codec, mvs_mode=mvs_mode, static_only=False, exclude_keyframes=True,
-        visu=True, debug=True, mode=x) for x in ["train", "val"]}
+        codec=codec, scales=scales, mvs_mode=mvs_mode, static_only=static_only,
+        exclude_keyframes=exclude_keyframes, visu=True, debug=True,
+        mode=x) for x in ["train", "val"]}
 
     dataloaders = {x: torch.utils.data.DataLoader(datasets[x], batch_size=batch_size,
         shuffle=False, num_workers=0) for x in ["train", "val"]}
-
-    stats = Stats()
 
     step_wise = True
 
@@ -370,9 +385,10 @@ if __name__ == "__main__":
             motion_vectors = motion_vectors.numpy()
             motion_vectors = (motion_vectors - np.min(motion_vectors)) / (np.max(motion_vectors) - np.min(motion_vectors))
 
-            motion_vectors = draw_boxes(motion_vectors, boxes_prev[:, 1:], None, color=(200, 200, 200))
-            motion_vectors = draw_boxes(motion_vectors, boxes[:, 1:], None, color=(255, 255, 255))
-            motion_vectors = draw_velocities(motion_vectors, boxes[:, 1:], velocities, scale=1000)
+            # draw boxes on motion vector image
+            #motion_vectors = draw_boxes(motion_vectors, boxes_prev[:, 1:], None, color=(200, 200, 200))
+            #motion_vectors = draw_boxes(motion_vectors, boxes[:, 1:], None, color=(255, 255, 255))
+            #motion_vectors = draw_velocities(motion_vectors, boxes[:, 1:], velocities, scale=1000)
 
             print("step: {}, MVS shape: {}".format(step, motion_vectors.shape))
 

lib/dataset/tools/compute_stats_new.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import torch
 
-from lib.dataset.dataset import MotionVectorDataset
+from lib.dataset.dataset_new import MotionVectorDataset
 
 
 class RunningStats():
@@ -32,7 +32,16 @@ class RunningStats():
 # run as python -m lib.dataset.tools.compute_stats from root dir
 if __name__ == "__main__":
     visu = False  # whether to show graphical output (frame + motion vectors) or not
-    dataset_train = MotionVectorDataset(root_dir='data', batch_size=1, codec="mpeg4", visu=visu, mode="train")
+    codec = "mpeg4"
+    mvs_mode = "dense"
+    static_only = True
+    exclude_keyframes = True
+    scales = [1.0, 0.75, 0.5]
+
+    dataset_train = MotionVectorDataset(root_dir='data', transforms=None, codec=codec,
+        scales=scales, mvs_mode=mvs_mode, static_only=static_only,
+        exclude_keyframes=exclude_keyframes, visu=visu, debug=False, mode="train")
+
     dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=1, shuffle=False, num_workers=0)
 
     step_wise = False
@@ -54,14 +63,10 @@ if __name__ == "__main__":
     for step, sample in enumerate(dataloader_train):
 
         motion_vectors = sample["motion_vectors"][0].numpy()
-        runnings_stats_mvs_x.update(np.mean(motion_vectors[:, :, 2]))
-        runnings_stats_mvs_y.update(np.mean(motion_vectors[:, :, 1]))
+        runnings_stats_mvs_x.update(np.mean(motion_vectors[0, :, :]))
+        runnings_stats_mvs_y.update(np.mean(motion_vectors[1, :, :]))
 
-        num_boxes_mask = sample["num_boxes_mask"]
-        velocities = sample["velocities"]
-        velocities = velocities[num_boxes_mask]
-        velocities = velocities.numpy()
-
+        velocities = sample["velocities"][0].numpy()
         for v_xc, v_yc, v_w, v_h in velocities:
             runnings_stats_vel_xc.update(v_xc)
             runnings_stats_vel_yc.update(v_yc)
@@ -69,6 +74,12 @@ if __name__ == "__main__":
             runnings_stats_vel_h.update(v_h)
 
         if visu:
+            motion_vectors = torch.from_numpy(motion_vectors)
+            motion_vectors = motion_vectors.permute(1, 2, 0)
+            motion_vectors = motion_vectors[..., [2, 1, 0]]
+            motion_vectors = motion_vectors.numpy()
+            motion_vectors = (motion_vectors - np.min(motion_vectors)) / (np.max(motion_vectors) - np.min(motion_vectors))
+
             print("step: {}, MVS shape: {}".format(step, motion_vectors.shape))
 
             cv2.imshow("frame", sample["frame"][0].numpy())
@@ -89,6 +100,8 @@ if __name__ == "__main__":
                         break
 
     # output finalized stats
+    print("codec = {}, mvs_mode = {}, static_only = {}, exclude_keyframes = {}, scales = {}".format(
+        codec, mvs_mode, static_only, exclude_keyframes, scales))
     print("mvs x -- mean: {}, variance: {}, std: {}".format(*runnings_stats_mvs_x.get_stats()))
     print("mvs y -- mean: {}, variance: {}, std: {}".format(*runnings_stats_mvs_y.get_stats()))
 

lib/models/pnet_upsampled.py
@@ -56,14 +56,15 @@ class PropagationNetwork(nn.Module):
         x = self.base(motion_vectors)
         x = self.conv1x1(x)
 
+        boxes_prev_ = boxes_prev.detach().clone()
         if num_boxes_mask is not None:
-            boxes_prev = boxes_prev[num_boxes_mask]
-        boxes_prev = boxes_prev.view(-1, 5)
-        boxes_prev = self._change_box_format(boxes_prev)
-        boxes_prev = self._frame_idx_to_batch_idx(boxes_prev)
+            boxes_prev_ = boxes_prev_[num_boxes_mask]
+        boxes_prev_ = boxes_prev_.view(-1, 5)
+        boxes_prev_ = self._change_box_format(boxes_prev_)
+        boxes_prev_ = self._frame_idx_to_batch_idx(boxes_prev_)
 
         # compute ratio of input size to size of base output
-        x = torchvision.ops.ps_roi_pool(x, boxes_prev, output_size=(self.POOLING_SIZE, self.POOLING_SIZE), spatial_scale=1/16)
+        x = torchvision.ops.ps_roi_pool(x, boxes_prev_, output_size=(self.POOLING_SIZE, self.POOLING_SIZE), spatial_scale=1/16)
         x = self.pooling(x)
         x = x.squeeze()
         velocities_pred = x.view(-1, 4)

train.py
@@ -129,12 +129,11 @@ def train(model, criterion, optimizer, scheduler, num_epochs=2, visu=False,
 
                 # log mean IoU of all predicted and ground truth boxes
                 if write_tensorboard_log:
-                    boxes_prev = boxes_prev[num_boxes_mask].detach()
-                    velocities_pred = velocities_pred.detach()
+                    boxes = boxes[num_boxes_mask].detach().view(-1, 5)
+                    boxes_prev = boxes_prev[num_boxes_mask].detach().view(-1, 5)
+                    velocities_pred = velocities_pred.detach().view(-1, 4)
                     boxes_pred = box_from_velocities(boxes_prev[:, 1:], velocities_pred)
-                    boxes = boxes[num_boxes_mask]
-                    boxes = boxes[:, 1:]
-                    mean_iou = compute_mean_iou(boxes_pred, boxes)
+                    mean_iou = compute_mean_iou(boxes_pred, boxes[:, 1:])
                     running_mean_iou.append(mean_iou)
                     writer.add_scalar('Mean IoU/{}'.format(phase), mean_iou, iterations[phase])
 
@@ -186,7 +185,7 @@ def train(model, criterion, optimizer, scheduler, num_epochs=2, visu=False,
 
 if __name__ == "__main__":
     root_dir = "data_precomputed"
-    train_parallel = True
+    train_parallel = False
     modes = ["train", "val"]
     datasets = {x: MotionVectorDatasetPrecomputed(root_dir=os.path.join(root_dir, x)) for x in modes}
     dataloaders = {x: torch.utils.data.DataLoader(datasets[x], batch_size=1, shuffle=True, num_workers=8) for x in modes}